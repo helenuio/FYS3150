b7a7534076b2b7d83073535d9dafbc256092c5ba
helenz@student.matnat.uio.no <helenzhuang98@outlook.com>
2020-09-07 16:02:15 +0200

    Eliminated need/use of g_tilde and d_tilde arrays

--- Prosjekt1/b_test.cpp
+++ Prosjekt1/b_test.cpp
@@ -22,8 +22,8 @@ inline double exact(double x)
 }
 
 int main(int argc, char* argv[])
-{ // argument count & argument vector
-  int exponent; // 10^exponent, exponent = 3
+{
+  int exponent; // 10^exponent
   string filename; // name of file taken from command line to read
   if( argc <= 1)
   {
@@ -41,17 +41,16 @@ int main(int argc, char* argv[])
       double* x = new double[n+1];        x[0] = 0;
       double* u = new double[n+1];
       double* g = new double[n+1];        g[0] = 0;
-      double* g_tilde = new double[n+1];  g_tilde[0] = 0;
-      double* b_tilde = new double[n+1];  b_tilde[0] = 0;
 
-      // elements along the matrix
-      double* a = new double[n+1];          a[0] = a[n+1] = 0;
-      double* b = new double[n+1];          b[0] = 0;
-      double* c = new double[n+1];          c[0] = c[n+1] = 0;
+      // arrays to contain elements along the diagonal and non-diagonal
+      double* a = new double[n+1];        a[0] = a[n+1] = 0;
+      double* b = new double[n+1];        b[0] = 0;
+      double* c = new double[n+1];        c[0] = c[n+1] = 0;
 
       double h = 1.0/(n);
       double hh = h*h;
 
+      // set the elements for our specific case
       for (int i = 1; i <= n+1; i++)
       {
         a[i] = c[i] = -1;
@@ -59,33 +58,35 @@ int main(int argc, char* argv[])
         x[i] = i*h;
         g[i] = hh*f(x[i]);
       }
-    // new filename
-    string fileout = filename;
-    // convert exponent to string
-    string exponent = to_string(i);
-    // filename-i (where i = exponent)
-    fileout.append(exponent);
 
-    g_tilde[1] = g[1];
-    b_tilde[1] = b[1];
+    // boundary conditions
+    u[0] = u[n+1] = 0.0;
 
     // forward substitution
     for (int i = 2; i <= n+1; i++)
     {
-      b_tilde[i] = b[i] - (c[i-1]*a[i-1])/b_tilde[i-1];
-      g_tilde[i] = g[i] - (g_tilde[i-1]*a[i-1])/b_tilde[i-1];
+      b[i] = b[i] - (c[i-1]*a[i-1])/b[i-1];
+      g[i] = g[i] - (g[i-1]*a[i-1])/b[i-1];
     }
-
-    u[0] = u[n+1] = 0.0; // boundary conditions
 
     // backward substitution
-    //u[n] = g_tilde[n]/b_tilde[n];
     for (int i = n; i >= 1; i--)
     {
-      u[i] = (g_tilde[i]-(c[i]*u[i+1]))/b_tilde[i];
+      u[i] = (g[i]-(c[i]*u[i+1]))/b[i];
     }
 
+    // filename for each exponent
+    string fileout = filename;
+
+    // convert exponent to string
+    string exponent = to_string(i);
+
+    // filename-i (where i = exponent)
+    fileout.append(exponent);
+
     ofile.open(fileout);
+
+    // write to file for easier plotting
     ofile << setiosflags(ios::showpoint | ios::uppercase);
     //      ofile << "       x:             approx:          exact:       relative error" << endl;
     for (int i = 1; i <= n; i++)
@@ -100,7 +101,7 @@ int main(int argc, char* argv[])
 
     ofile.close();
     delete [] x; delete [] b; delete [] a;
-    delete [] c; delete [] u; delete [] g_tilde; delete [] b_tilde;
+    delete [] c; delete [] u; delete [] g;
   } // function looping over n=0,1,2
 
 return 0;
