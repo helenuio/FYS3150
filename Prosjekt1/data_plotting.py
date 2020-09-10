22f8363ddade307e6329631094128f666ed5a965
helenuio <70015253+helenuio@users.noreply.github.com>
2020-09-10 22:16:11 +0200

    Add files via upload

--- Prosjekt1/data_plotting.py
+++ Prosjekt1/data_plotting.py
@@ -1,18 +1,21 @@
 import matplotlib.pyplot as plt
+import sys
 
-myfile = open("c-file1", "r")
-
-n = []
-e = []
-x = []
-for i in myfile:
-    i = i.split()
-    x.append(i[0])
-    n.append(i[1])
-    e.append(i[2])
-
-plt.plot(x,n, label="numerisk")
-plt.plot(x,e, label="eksakt")
-plt.legend()
-plt.show()
-myfile.close()
+if len(sys.argv) < 1:
+    print("Please write the name of the file you wish to plot")
+else:
+    filename = str(sys.argv[1])
+    myfile = open(filename, "r")
+    e = []
+    x = []
+    n = []
+    for i in myfile:
+        i = i.split()
+        x.append(i[0])
+        n.append(i[1])
+        e.append(i[2])
+    plt.plot(x,n, label="numerisk")
+    plt.plot(x,e, label="eksakt")
+    plt.legend()
+    plt.show()
+    myfile.close()
